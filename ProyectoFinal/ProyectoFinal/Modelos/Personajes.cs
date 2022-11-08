f725cf1d588e5de664c644ddbc4444a687aac1cf
angel rivas nieto <anxo111@hotmail.com>
2022-11-08 19:54:47 +0100

    Pantalla Raza terminada

--- ProyectoFinal/ProyectoFinal/Modelos/Personajes.cs
+++ ProyectoFinal/ProyectoFinal/Modelos/Personajes.cs
@@ -6,6 +6,306 @@ namespace ProyectoFinal.Modelos
 {
     class Personajes
     {
+        string Informacion = "";
+        //Puntuacion de caracteristicas
+        int fuerza = 0;
+        int contitucion = 0;
+        int destreza = 0;
+        int inteligencia = 0;
+        int sabiduria = 0;
+        int carisma = 0;
+        //Defensas
+        int fortaleza = 0;
+        int refejos = 0;
+        int voluntad = 0;
+        int puntos_de_golpe = 0;
+        int ca = 0;
+        //armadura
+        string armadura = "";
+        //Habilidades
+        int atletismo = 0;
+        int resistencia = 0;
+        int sanar = 0;
+        int intimidar = 0;
+        int acrobacias = 0;
+        int aguante = 0;
+        int arcanos = 0;
+        int diplomacia = 0;
+        int dungeos = 0;
+        int engañar = 0;
+        int historia = 0;
+        int naturaleza = 0;
+        int percepcion = 0;
+        int perpicacia = 0;
+        int recursos = 0;
+        int sigilo = 0;
+        int religion = 0;
+
+        Personajes crearPersonaje(string raza,string clase)
+        {
+            Personajes personaje = new Personajes();
+            switch (raza)
+            {
+                case "Humano":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            //HUMANO BRUJO
+                            personaje.Informacion = "Hechizos a voluntad:    \r\nEvocaciones a voluntad: Eldritch Blast Hellish Rebuke Dire Radiance\r\nNivel 1 diario: maldición del sueño oscuro\r\nEncuentro de nivel 1: palabra espantosa\r\n Dotes: 2 hazañas heroicas  ";
+                            //Puntuacion de caracteristicas
+                            personaje.fuerza = 12;
+                            personaje.contitucion = 14;
+                            personaje.destreza = 8;
+                            personaje.inteligencia = 16;
+                            personaje.sabiduria = 14;
+                            personaje.carisma = 14;
+                            //Defensas
+                            personaje.fortaleza = 13;
+                            personaje.refejos = 15;
+                            personaje.voluntad = 14;
+                            personaje.puntos_de_golpe = 26;
+                            personaje.ca = 11;
+                            //armadura
+                            personaje.armadura = "ninguan";
+                            //Habilidades
+                            personaje.atletismo = 2;
+                            personaje.resistencia = 2;
+                            personaje.sanar = 2;
+                            personaje.intimidar = 2;
+                            personaje.acrobacias = 2;
+                            personaje.aguante = 2;
+                            personaje.arcanos = 8;
+                            personaje.diplomacia = 2;
+                            personaje.dungeos = 2;
+                            personaje.engañar = 7;
+                            personaje.historia = 8;
+                            personaje.naturaleza = 2;
+                            personaje.percepcion = 2;
+                            personaje.perpicacia = 7;
+                            personaje.recursos = 2;
+                            personaje.religion = 8;
+                            personaje.sigilo = 2;
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            //HUMANO GUERERO
+                            personaje.Informacion = "";
+                            //Puntuacion de caracteristicas
+                            personaje.fuerza = 16;
+                            personaje.contitucion = 16;
+                            personaje.destreza = 10;
+                            personaje.inteligencia = 12;
+                            personaje.sabiduria = 12;
+                            personaje.carisma = 10;
+                            //Defensas
+                            personaje.fortaleza = 16;
+                            personaje.refejos = 12;
+                            personaje.voluntad = 12;
+                            personaje.puntos_de_golpe = 31;
+                            personaje.ca = 17;
+                            //armadura
+                            personaje.armadura = "escamas";
+                            //Habilidades
+                            personaje.atletismo = 8;
+                            personaje.resistencia = 8;
+                            personaje.sanar = 6;
+                            personaje.intimidar = 5;
+                            personaje.acrobacias = 2;
+                            personaje.aguante = 2;
+                            personaje.arcanos = 2;
+                            personaje.diplomacia = 2;
+                            personaje.dungeos = 2;
+                            personaje.engañar = 2;
+                            personaje.historia = 2;
+                            personaje.naturaleza = 2;
+                            personaje.percepcion = 2;
+                            personaje.perpicacia = 2;
+                            personaje.recursos = 2;
+                            personaje.sigilo = 2;
+                            personaje.religion = 2;
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Elfo":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Enano":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Eladrines":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Draconidos":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Semielfo":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Mediano":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Tiflin":
+                    switch (clase)
+                    {
+                        case "Brujo":
+                            break;
+                        case "Clerigo":
+                            break;
+                        case "Explorador":
+                            break;
+                        case "Guerrero":
+                            break;
+                        case "Mago":
+                            break;
+                        case "Paladin":
+                            break;
+                        case "Picaro":
+                            break;
+                        case "Señor de la guerra":
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return personaje;
+        }
         public class Humano : Personajes
         {
             public class Guerrero: Humano
--- ProyectoFinal/ProyectoFinal/Vistas/Raza.xaml.cs
+++ ProyectoFinal/ProyectoFinal/Vistas/Raza.xaml.cs
@@ -12,11 +12,19 @@ namespace ProyectoFinal.Vistas
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Raza : ContentPage
     {
+        string raza = "";
         public Raza()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
             pikerRaza.SelectedIndex = 0;
             pikerRaza.SelectedItem = pikerRaza.Items.ElementAt(0);
+            //imageRaza.Source = ImageSource.FromFile("Humano.png");
+            //imageRaza.IsVisible = false;
+            
+            
+
+
         }
 
         private void pikerRaza_SelectedIndexChanged(object sender, EventArgs e)
@@ -25,27 +33,53 @@ namespace ProyectoFinal.Vistas
             switch (pikerRaza.SelectedItem.ToString())
             {
                 case "Humano":
+                    imageRaza.Source = ImageSource.FromFile("Humano.png");
                     txtInfo.Text = "De todas las razas civilizadas, los humanos son los más adaptables y variados. Es posible encontrar asentamientos humanos en cualquier parte, y la moralidad, costumbres e intereses de esta raza varían enormemente."+
                         "Juega con un humano si quieres…\r\n\r\n✦ Ser un héroe decidido e ingenioso con suficiente determinación como para enfrentarte a cualquier desafío.\r\n✦ Tener la mayor versatilidad y flexibilidad de todas las razas.\r\n✦ Ser capaz de destacar en cualquier clase que elijas.";
+                    raza = "Humano";
                     break;
                 case "Elfo":
+                    imageRaza.Source = ImageSource.FromFile("Elfos.png");
                     txtInfo.Text = "Salvajes y libres, los elfos custodian sus tierras boscosas empleando el sigilo y mortíferas flechas que descienden desde los árboles. Construyen sus hogares en armonía con el bosque, tan perfectamente unidos, que los viajeros a menudo no descubren que han entrado en una comunidad élfica hasta que es demasiado tarde\r\n\r\n"+
                         "Juega con un elfo si quieres…\r\n\r\n✦ Ser rápido, silencioso y salvaje.\r\n✦ Dirigir a tus compañeros a través de los bosques más profundos y atravesar a tus enemigos con flechas.\r\n✦ Ser miembro de una raza que prefiere las clases explorador, pícaro y clérigo.";
+                    raza = "Elfo";
                     break;
                 case "Enano":
-                    txtInfo.Text = "";
+                    imageRaza.Source = ImageSource.FromFile("Enanos.png");
+                    txtInfo.Text = "Tallados de los cimientos de roca del universo, los enanos soportaron una era de servidumbre bajo los gigantes antes de ganar la libertad. Sus poderosas ciudades fortificadas de las montañas son testimonio del poder de sus antiguos imperios. Incluso aquellos que viven en las ciudades humanas, se cuentan entre los defensores más firmes contra la oscuridad que amenaza con devorar el mundo."+
+                        "\r\n\r\nJuega con un enano si quieres...\r\n\r\n✦ Ser duro, rudo y fuerte como una roca.\r\n✦ Aumentar la gloria de tus antepasados o ser la mano derecha de tu dios.\r\n✦ Ser capaz de soportar tanto daño como eres capaz de causar.\r\n✦ Ser miembro de una raza que prefiere las clases paladín,clérigo y guerrero.";
+
+                    raza = "Enano";
                     break;
                 case "Draconido":
-                    txtInfo.Text = "";
+                    imageRaza.Source = ImageSource.FromFile("Draconidos.png");
+                    txtInfo.Text = "Nacidos para luchar, los dracónidos son una raza errante de mercenarios, soldados y aventureros. Hace mucho tiempo, su imperio aspiró a dominar todo el mundo, pero ahora sólo unos pocos clanes desarraigados de estos honorables luchadores resisten para transmitir las leyendas de su antigua gloria.\r\n\r\n"+
+                        "Juega con un dracónido si quieres...\r\n\r\n✦ Tener el aspecto de un dragón.\r\n✦ Ser el orgulloso heredero de un antiguo imperio caído.\r\n✦ Exhalar ácido, frío, fuego, relámpago o veneno.\r\n✦ Ser miembro de una raza que prefiere las clases señor de la guerra, guerrero y paladín.";
+                    raza = "Draconido";
                     break;
                 case "Mediano":
-                    txtInfo.Text = "";
+                    imageRaza.Source = ImageSource.FromFile("Medianos.png");
+                    txtInfo.Text = "Los medianos son una raza pequeña, conocida por su ingenio, su veloz astucia y sus nervios de acero. Son un pueblo nómada que recorre los cursos de agua y los pantanos. Nadie ha viajado más lejos ni ha visto más de lo que sucede en el mundo que los medianos.\r\n\r\n"+
+                        "Juega con un mediano si quieres...\r\n\r\n✦ Ser un héroe valeroso al que es fácil subestimar.\r\n✦ Ser amable, afectuoso y amistoso.\r\n✦ Ser miembro de una raza que prefiere las clases pícaro, explorador y señor de la guerra";
+                    raza = "Mediano";
                     break;
                 case "Semielfo":
-                    txtInfo.Text = "";
+                    imageRaza.Source = ImageSource.FromFile("Semielfos.png");
+                    txtInfo.Text = "Descendientes de elfos y humanos, los semielfos son una raza llena de vida en la cual suelen aparecer los mejores rasgos de elfos y humanos.\r\n\r\n"+
+                        "Juega con un semielfo si quieres...\r\n\r\n✦ Ser un líder extrovertido y entusiasta.\r\n✦ Ser un héroe carismático que se siente en casa por igual en dos culturas diferentes.\r\n✦ Ser miembro de una raza que prefiere las clases señor de la guerra, paladín y brujo.";
+                    raza = "Semielfo";
                     break;
                 case "Tiflin":
-                    txtInfo.Text = "";
+                    imageRaza.Source = ImageSource.FromFile("Tiflin.png");
+                    txtInfo.Text = "Herederos de un antiguo linaje infernal, los tiflin no tienen reinos propios, sino que viven dentro de los reinos y ciudades humanas. Son descendientes de nobles humanos que negociaron con poderes oscuros, y hace mucho tiempo su imperio subyugó la mitad del mundo. Pero el imperio se derrumbó, y los tiflin fueron abandonados para encontrar su propio camino en un mundo que a menudo les teme y está resentido con ellos\r\n"+
+                        "Juega con un tiflin si quieres...\r\n✦ Ser un héroe con un lado oscuro al que derrotar.\r\n✦ Ser bueno engañando, intimidando o persuadiendo a otros para que hagan tu voluntad.\r\n✦ Ser miembro de una raza que prefiere las clases brujo, señor de la guerra y pícaro";
+                    raza = "Tiflin";
+                    break;
+                case "Eladrines":
+                    imageRaza.Source = ImageSource.FromFile("Eladrines.png");
+                    txtInfo.Text = "Los eladrines son criaturas de magia con fuertes lazos con la Naturaleza, que viven en ciudades en el reino crepuscular de las tierras Salvajes de las hadas. Sus ciudades se encuentran tan próximas al mundo, que a veces cruzan la frontera, apareciendo brevemente en valles de montaña o en profundos claros de bosque antes de desaparecer de nuevo en las tierras Salvajes de las hadas.\r\n\r\n"+
+                        "Juega con un eladrín si quieres...\r\n✦ Ser de otro mundo y misterioso.\r\n✦ Ser grácil e inteligente.\r\n✦ Teleportarte por el campo de batalla envuelto en la magia de las tierras Salvajes de las hadas.\r\n✦ Ser miembro de una raza que prefiere las clases mago,pícaro y señor de la guerra.";
+                    raza = "Eladrines";
                     break;
                 default:
                     break;
